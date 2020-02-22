803a693cc9e378ebc1ccfc3bc2213ca6238fa159
derek-cornelison <dcornelison1216@gmail.com>
2020-02-21 22:36:48 -0500

    Changed functions inside functions.js to arrow syntax

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,9 +6,7 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-function consume(a, b, cb) {
-  return cb(a, b);
-}
+const consume = (a, b, cb) => cb(a, b);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -16,15 +14,9 @@ function consume(a, b, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(num1, num2) {
-  return num1 + num2;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function greeting(fname, lname) {
-  return `Hello ${fname} ${lname}, nice to meet you!`;
-}
+const add = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
+const greeting = (fname, lname) => `Hello ${fname} ${lname}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
